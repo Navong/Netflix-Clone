240eb56f3a9401cde14734a6e9c522f7391a1c25
Bong Channavong <bongchannavong@outlook.com>
2023-01-15 01:52:29 +0900

    nexflix-clone

---
+++ pages/MyComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BeakerIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+export default function MyComponent() {
+  return (
+    <div>
+      <BeakerIcon className="h-6 w-6 text-blue-500"/>
+      <MagnifyingGlassIcon />
+      <p>...</p>
+    </div>
+  )
+}
